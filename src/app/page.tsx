9f06a659b8b8abc711afe04acb082721a629bd03
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-04-23 22:30:22 +0100

    Getting mock data images from uploadthing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/2ac65303-81a5-4f78-8ea5-b70d988b5c05-llljzy.jpg",
+  "https://utfs.io/f/d3295db1-e664-46a5-8d5b-f592555ca5ec-lkegh4.jpg",
+  "https://utfs.io/f/2aa1c9bd-807d-4d4f-8cb7-5891b4cfefbd-jvefp9.jpg",
+  "https://utfs.io/f/76f55a44-3838-4d1e-b479-f8d06b893f5d-83qoe6.jpg",
+  "https://utfs.io/f/f4380a97-c5c3-4ae0-8513-edad8400089a-3ax2qh.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (Photo Gallery in progress)
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
