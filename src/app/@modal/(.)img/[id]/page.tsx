525fa0dce7c413f723b379706bd4e3776e386a1a
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-05-03 19:33:43 +0100

    Start implementing parallel routes

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,7 +1,18 @@
-export default function PhotoModal({
+import { getImage } from "~/server/queries";
+
+export default async function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  return <div>{photoId}</div>;
+  const idAsNumber = Number(photoId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+  const image = await getImage(idAsNumber);
+  return (
+    <div>
+      <img src={image.url} className="w-96" />
+      {photoId}
+    </div>
+  );
 }
