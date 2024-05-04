15b779d57e270e7e110ee8759ad054e75867e110
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-05-04 12:22:42 +0100

    more cleanup on modal

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -2,5 +2,19 @@ import { getImage } from "~/server/queries";
 
 export default async function FullImagePageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img
+          src={image.url}
+          alt={image.name}
+          className="flex-shrink object-contain"
+        />
+        ;
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <p className="text-xl font-bold">{image.name}</p>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 import { withUt } from "uploadthing/tw";
 
 export default withUt({
-  content: ["./src/**/*.{ts, tsx, mdx}"],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
